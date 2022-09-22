0e5a2a4e730cd0f0260981ca26ea8f69c8120187
pedroborge <pedroborges@usp.br>
2022-09-22 16:59:35 -0300

    task

--- tasks/models.py
+++ tasks/models.py
@@ -1,20 +1,20 @@
 from django.db import models
-
-# Create your models here.
-STATUS = (
-    ("doing", "Doing"),
-    ('done', "Done"),
-)
-
+from django.contrib.auth import get_user_model
 
 class Task(models.Model):
+
+    STATUS = (
+        ('doing', 'Doing'),
+        ('done', 'Done'),
+    )
+
     title = models.CharField(max_length=255)
     description = models.TextField()
     done = models.CharField(
         max_length=5,
         choices=STATUS,
-        
     )
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
